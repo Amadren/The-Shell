108d7b3933270080cc2828ecd5c494a0cb43541c
Amadren <sazert103@californiacolleges.edu>
2016-07-27 10:00:20 +0200

    Update shell.js

--- assets/js/shell.js
+++ assets/js/shell.js
@@ -1,38 +1,26 @@
-/*$(function() {
-    // Stick the #nav to the top of the window
-    var nav = $('#navWrap');
-    var navHomeY = $('#navWrap').offset().top - parseFloat($('#navWrap').css('marginTop').replace(/auto/, 100));
-    var isFixed = false;
-    var $w = $(window);
-    $w.scroll(function() {
-        var scrollTop = $w.scrollTop();
-        var shouldBeFixed = scrollTop > navHomeY;
-        if (shouldBeFixed && !isFixed) {
-            $('#navWrap').addClass('fixed');
-
-            isFixed = true;
-        }
-        else if (!shouldBeFixed && isFixed)
-        {
-            nav.css({
-                position: 'static'
-            });
-            isFixed = false;
-        }
-    });
+$(function() {
+ var nav = $('#nav');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
 });
-*/
-
-//TEST
-$(document).ready(function () {  
-  var top = $('#navWrap').offset().top;
-  $(window).scroll(function (event) {
-    var y = $(this).scrollTop();
-    if (y >= top) {
-      $('#navWrap').addClass('fixed');
-    } else {
-      $('#navWrap').removeClass('fixed');
-    }
-  });
-});
-
